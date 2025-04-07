4392b675ac153525b4a527bbedfbfd99af9a016e
Juliano Campos dos Santos <juliano.kampus@gmail.com>
2025-04-07 12:24:06 -0300

    sb

---
+++ model/partidos.js
@@ -0,0 +1,9 @@
+export default class Partido {
+    
+    constructor(nome, sigla, numero, id) {
+        this.nome = nome;
+        this.sigla = sigla;
+        this.numero = numero;
+        this.id = id;
+    }
+}
