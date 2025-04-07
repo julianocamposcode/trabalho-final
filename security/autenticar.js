745a1d672a3b685d0cf98311d8f66a03678a5ff0
Juliano Campos dos Santos <juliano.kampus@gmail.com>
2025-04-07 12:10:20 -0300

    backend

--- security/autenticar.js
+++
@@ -1,8 +0,0 @@
-export default function autenticar(req, res, next) {
-    if (req.session.autenticado === true) {
-        next();
-    } else {
-        req.session.urlOriginal = req.originalUrl;
-        res.redirect('/login');
-    }
-}
